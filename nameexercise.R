0df8a479f6644f9c40aff3e4579f1e4fc33a5984
Kevin Vo <khvo1402@uw.edu>
2020-11-12 00:34:55 -0800

    loading too much

---
+++ nameexercise.R
@@ -0,0 +1,56 @@
+# Exercise 5: large data sets: Baby Name Popularity Over Time
+
+# Read in the female baby names data file found in the `data` folder into a 
+# variable called `names`. Remember to NOT treat the strings as factors!
+names <- read.csv("data/female_names.csv", stringsAsFactors = FALSE)
+
+View(names)
+# Create a data frame `names_2013` that contains only the rows for the year 2013
+names_2013 <- names[names$year == 2013, ]
+
+View(names_2013)
+
+# What was the most popular female name in 2013?
+most_pop_name <- names_2013[names_2013$prop == max(names_2013$prop), "name"]
+
+# least popular in 2013
+least_pop_2013 <- names_2013[names_2013$prop == min(names_2013$prop), "name"]
+
+#most_pop_name <- max(names_2013[names_2013$prop == 2013,]) doesn't work?
+
+# Write a function `most_popular_in_year` that takes in a year as a value and 
+# returns the most popular name in that year
+most_popular_in_year <- function(year) {
+  names_year <- names[names$year == year, ]
+  most_popin <- names_year[names_year$prop == max(names_year$prop), "name"]
+  return(most_popin)
+}
+
+# What was the most popular female name in 1994?
+most_popular_in_year(1994)
+
+#im making a least popular because yes
+least_popular_in_year <- function(year) {
+  names_year <- names[names$year == year, ]
+  least_popin <- names_year[names_year$prop == min(names_year$prop), "name"]
+  return(least_popin)
+}
+
+test <- least_popular_in_year(1994)
+# Write a function `number_in_million` that takes in a name and a year, and 
+# returns statistically how many babies out of 1 million born that year have 
+# that name. 
+# Hint: get the popularity percentage, and take that percentage out of 1 million.
+number_in_million <- function(name, year) {
+  name_popularity <- names[names$year == year & names$name == name, "prop"]
+  round(name_popularity * 100000, 1)
+}
+
+# How many babies out of 1 million had the name 'Laura' in 1995?
+laura_1995 <- number_in_million("Laura", 1995)
+
+# How many babies out of 1 million had your name in the year you were born?
+best_name <- number_in_million("Kevin", 2002)
+kevin_2013 <- number_in_million("Kevin", 2013)
+## Consider: what does this tell you about how easy it is to identify you with 
+## just your name and birth year?
